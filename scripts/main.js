7c0a91996b84f6d0612d81afb8799caed1a2edd4
Jahongir95 <sjahongir0032@gmail.com>
2021-02-05 18:09:46 +0500

    html is creating

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let quizQuestion = document.querySelector('#quiz__question');
 let quizOptions = document.querySelector('#quiz__options');
 
 function aTagsCreator(){
-    // for(i = 1; i <= 20; i++){
+    for(i = 1; i <= 20; i++){
         let aTag = document.createElement('a');
         let image = document.createElement('img');
         aTag.setAttribute('class', 'question-area');
@@ -11,18 +11,7 @@ function aTagsCreator(){
         quizOptions.appendChild(aTag);
         aTag.appendChild(image);
         console.log(5);
-    // }
+    }
 }
 
-var quizNum = 0;
-let time = setInterval(() => {
-    aTagsCreator();
-
-    if(quizNum > 20){
-        clearInterval(time);
-    } else {
-        quizNum++;
-    }
-    console.log(quizNum);
-}, 500);
-
+aTagsCreator();
