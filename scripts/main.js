f5efc532ccae817e5a1b62952552d5c89711c25b
Jahongir95 <sjahongir0032@gmail.com>
2021-02-05 15:08:51 +0500

    Initial step of the form is finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
 let quizQuestion = document.querySelector('#quiz__question');
 let quizOptions = document.querySelector('#quiz__options');
 
-for(i = 1; i <= 20; i++){
-    let aTag = document.createElement('a');
-    let image = document.createElement('img');
-    aTag.setAttribute('class', 'question-area');
-
-
-    quizOptions.appendChild(aTag);
-    aTag.appendChild('image');
+function aTagsCreator(){
+    for(i = 1; i <= 20; i++){
+        let aTag = document.createElement('a');
+        let image = document.createElement('img');
+        aTag.setAttribute('class', 'question-area');
+    
+        console.log(aTag);
+        quizOptions.appendChild(aTag);
+        aTag.appendChild(image);
+    }
 }
+
+// let time = setInterval(() => {
+    aTagsCreator();
+    
+// }, 500);
+
