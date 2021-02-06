c3689a98356b39c004c13fa8dd8a0e6a857ea8b6
Jahongir95 <sjahongir0032@gmail.com>
2021-02-06 22:50:42 +0500

    start button function is created

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,29 @@
+// savol chiqadigan maydon
 let quizQuestion = document.querySelector('#quiz__question');
+// savollar yachaykasi bu
 let quizOptions = document.querySelector('#quiz__options');
+// start buttondir bu
+let startBtn = document.querySelector('#start-btn');
+// section 1 class bilan chaqiriladi
+let sectionOne = document.querySelector('.section1');
+// section 2 class bilan chaqiriladi
+let sectionTwo = document.querySelector('.section2');
 
+//
+startBtn.addEventListener('click', function(){
+    sectionOne.classList.remove('active');  
+    sectionTwo.classList.add('active');  
+})
+
+// rasm chiqadigan maydonlarni yaratadigan function bu - 
 function aTagsCreator(){
     for(i = 1; i <= 20; i++){
         let aTag = document.createElement('a');
         let image = document.createElement('img');
         aTag.setAttribute('class', 'question-area');
     
-        console.log(aTag);
         quizOptions.appendChild(aTag);
         aTag.appendChild(image);
-        console.log(5);
     }
 }
 
